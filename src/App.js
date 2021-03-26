279c0a87b3f493c0173da4e49ea13acc671191c5
Aria <aria.abad2030@yahoo.com>
2021-03-25 20:54:38 -0700

    set the board

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { useState } from "react";
 
 function App() {
+
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
+
   return (
     <div className="App">
-      <h1>Tic Tac Toe</h1>
+      <h1>Tic Tac Toe game app</h1>
     </div>
   );
 }
