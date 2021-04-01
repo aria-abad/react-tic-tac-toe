f917450c739853aef49db7a1dcdf3db4615a32b9
Aria <aria.abad2030@yahoo.com>
2021-03-31 21:29:34 -0700

    updating the HTML class

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import { useState } from "react";
 
 function App() {
 
-  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""])
+  const [board, setBoard] = useState(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
 
+  function testFunc(id) {
+    console.log("test")
+    console.log("id:", id)
+    console.log(board)
+    // setBoard( ...board )
+    console.log(board[id])
+  }
+  
   return (
     <div className="App">
       <div className="board">
         {board.map((square, id) => {
-          return (<div className="square" key={id} onClick={() => console.log(id)} > {square} </div>)
+          return (<div className={`square square${id}`} key={id} onClick={() => testFunc(id)}> {square} </div>)
         })}
       </div>
     </div>
