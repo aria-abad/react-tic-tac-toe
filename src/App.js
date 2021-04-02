1b9259baf065ca3c44cc7e484cd6837001211ee8
Aria <aria.abad2030@yahoo.com>
2021-04-01 22:01:05 -0700

    change div to button in main display

--- src/App.js
+++ src/App.js
@@ -5,18 +5,21 @@ function App() {
   const [board, setBoard] = useState(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
 
   function testFunc(id) {
-    console.log("test")
     console.log("id:", id)
     console.log(board)
-    // setBoard( ...board )
     console.log(board[id])
+    let temp = board;
+    temp[id] = 'x';
+    console.log(temp);
+    setBoard(temp);
+    console.log(board);
   }
   
   return (
     <div className="App">
       <div className="board">
         {board.map((square, id) => {
-          return (<div className={`square square${id}`} key={id} onClick={() => testFunc(id)}> {square} </div>)
+          return (<button className={`square square${id}`} key={id} onClick={() => testFunc(id)}> {square} </button>)
         })}
       </div>
     </div>
