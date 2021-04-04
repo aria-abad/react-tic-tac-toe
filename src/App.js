02cdfa9bc7caf8d859f6daf6dfddb507cda6cd88
Aria <aria.abad2030@yahoo.com>
2021-04-03 22:59:22 -0700

    update setboard function

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ function App() {
 
   const [board, setBoard] = useState(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
 
-  const testFunc = id => {
-    console.log("id:", id)
-    console.log(board)
-    console.log(board[id])
-    let temp = board;
-    temp[id] = 'x';
-    console.log(temp);
-    setBoard(temp);
-    console.log(board);
-    // window.location.reload();
-  }
+  // const testFunc = id => {
+  //   console.log("id:", id)
+  //   console.log(board)
+  //   let temp = board;
+  //   temp[id] = 'x';
+  //   console.log(temp);
+  //   setBoard(temp);
+  //   console.log(board);
+  //   // window.location.reload();
+  // }
   
   return (
     <div className="App">
       <div className="board">
         {board.map((square, id) => {
-          return (<button className={`square square${id}`} key={id} onClick={() => testFunc(id)}> {square} </button>)
+          return (<button className={`square square${id}`} key={id} onClick={() => setBoard([board[id]='x'])}> {square} </button>)
         })}
       </div>
     </div>
