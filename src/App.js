a24dbff9fef9af8257f3a49cd0e6bcbc4038f52f
Aria <aria.abad2030@yahoo.com>
2021-04-05 20:39:04 -0700

    writing x or o in the cell

--- src/App.js
+++ src/App.js
@@ -3,26 +3,17 @@ import { useState } from "react";
 function App() {
 
   const [board, setBoard] = useState(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
-
-  // const testFunc = id => {
-  //   console.log("id:", id)
-  //   console.log(board)
-  //   let temp = board;
-  //   temp[id] = 'x';
-  //   console.log(temp);
-  //   setBoard(temp);
-  //   console.log(board);
-  //   // window.location.reload();
-  // }
+  const [xo, setXo] = useState('X');
   
   return (
     <div className="App">
       <div className="board">
         {board.map((square, id) => {
-          return (<button className={`square square${id}`} key={id} onClick={() => {
-            board[id] = 'x'
+          return (<div className={`square square${id}`} key={id} onClick={() => {
+            board[id] = xo;
             setBoard([...board])
-          }}> {square} </button>)
+            xo === 'X' ? setXo('O') : setXo('X')
+          }}> {square} </div>)
         })}
       </div>
     </div>
