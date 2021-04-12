97c12521b82641a7a5830977c5a78c99b7147ebd
Aria <aria.abad2030@yahoo.com>
2021-04-11 22:36:58 -0700

    update the initial board array

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function App() {
 
-  const [board, setBoard] = useState(["0", "1", "2", "3", "4", "5", "6", "7", "8"])
+  const [board, setBoard] = useState(['', '', '', '', '', '', '', '', ''])
   const [XO, setXO] = useState('X');
   
   return (
